abfa2da7e20b6830a97620f151e7f9354aab208f
xuh <xuh-global@qq.com>
2019-04-10 10:40:04 +0800

    123

--- src/main/java/test/Happy.java
+++ src/main/java/test/Happy.java
@@ -3,6 +3,6 @@ package test;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("test.......");
-		System.out.println("commit...right");
+		System.out.println("commit...left");
 	}
 }
